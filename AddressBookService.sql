36dc07d2cbe53bd8c6226546494672d0fbbd1ded
Viraj <jadhavviraj50@gmail.com>
2022-06-03 17:33:53 +0530

    [Viraj]-Added UC2-CreateAddressBookTable file

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -6,3 +6,18 @@ Create Database AddressBookService;
 Use AddressBookService;
 
 
+UC2 : Create Address Book Table
+
+
+CREATE TABLE AddressBook
+(
+Id INT PRIMARY KEY IDENTITY (1,1),
+FirstName VARCHAR(100),
+LastName VARCHAR(100),
+Address VARCHAR(500),
+City VARCHAR(50),
+State VARCHAR(50),
+Zip INT,
+PhoneNumber VARCHAR(10),
+Email VARCHAR(100)
+);
